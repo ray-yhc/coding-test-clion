8d446e880904c0307b9e0a5a385ac6b35ea20bcf
raycho <raycho521@yonsei.ac.kr>
2023-05-15 11:47:35 +0900

    feat: add new problem

---
+++ BJ_4673.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+vector<int> getList(int N) {
+    vector<int> list;
+    for (int number = 1; number <= N; ++number) {
+//        int n = number - 1;
+        int dn = 0;
+        for (int n = number-1; n > 0; --n) {
+            dn = n +
+                 n / 1000 +
+                 (n % 1000) / 100 +
+                 (n % 100) / 10 +
+                 (n % 10);
+
+            if (dn == number)
+                break;
+        }
+
+        if (dn == number)
+            continue;
+        else
+            list.push_back(number);
+    }
+    return list;
+}
+
+int main() {
+//    freopen("./txts/1009.txt", "rt", stdin);
+    vector<int> list = getList(10000);
+
+    for (int i: list) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
