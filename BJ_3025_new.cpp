cff4b28b7af8484e911e7b64ba76d8dbd85020d1
ray-yhc <raycho521@yonsei.ac.kr>
2023-06-24 16:04:14 +0900

    feat: add new problem

--- BJ_3025_new.cpp
+++ BJ_3025_new.cpp
@@ -11,27 +11,89 @@ stack<pair<int, int>> checkpoint[31]; // checkpoint[col] == {row, col}
 set<int> obstacles[31];
 
 
+void addStone(int r, int c) {
+//    assert(gameMap[r][c] == '.');
+    gameMap[r][c] = 'O';
+    obstacles[c].emplace(r);
+}
+
+bool isEmpty(int r, int c) {
+    if (r < 1 || r > N || c < 1 || c > M || gameMap[r][c] != '.') return false;
+    return true;
+}
+
 int max(int a, int b) { return (a > b) ? a : b; }
+
 int min(int a, int b) { return (a < b) ? a : b; }
 
 int main() {
-    cin >> N >> M;
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < M ; ++j) {
-            char cc;
-            cin >> cc;
-            gameMap[i][j] = cc;
 
-            if (cc != '.') {
+    scanf("%d %d", &N, &M);
+    for (int i = 1; i <= N; ++i) {
+        for (int j = 1; j <= M; ++j) {
+            char cc[2];
+            scanf("%1s", &cc);
+            gameMap[i][j] = cc[0];
+
+            if (cc[0] != '.') {
                 obstacles[j].emplace(i);
+            }
+        }
+        getc(stdin);
+    }
+
+
+    scanf("%d", &D);
+    for (int i = 0; i < D; ++i) {
+        int dropColumn;
+        scanf("%d", &dropColumn);
+
+        // init
+        int c = dropColumn;
+        int r = 1;
+
+        while (!checkpoint[dropColumn].empty()) {
+            if (gameMap[checkpoint[dropColumn].top().first][checkpoint[dropColumn].top().second] != '.') {
+                checkpoint[dropColumn].pop();
+            } else {
+                r = checkpoint[dropColumn].top().first;
+                c = checkpoint[dropColumn].top().second;
+                break;
+            }
+        }
+
+        while (true) {
+            if (obstacles[c].upper_bound(r) != obstacles[c].end()) {
+                r = *(obstacles[c].upper_bound(r)) - 1;
+            }
+            else r = N;
+//            assert(isEmpty(r, c));
+
+            // X or end
+            int nr = r + 1, nc = c;
+            if (nr > N || gameMap[nr][nc] == 'X') {
+                addStone(r, c);
+                break;
+            }
+
+            //
+            if (isEmpty(r, c - 1) && isEmpty(r + 1, c - 1)) {
+                checkpoint[dropColumn].emplace(r, c);
+                r = r + 1;
+                c = c - 1;
+            } else if (isEmpty(r, c + 1) && isEmpty(r + 1, c + 1)) {
+                checkpoint[dropColumn].emplace(r, c);
+                r = r + 1;
+                c = c + 1;
+            } else {
+                addStone(r, c);
+                break;
             }
         }
     }
 
-    cin >> D;
-    for (int i = 0; i < D; ++i) {
-
-        //
+    for (int i = 1; i <= N; ++i) {
+        printf("%s\n", gameMap[i] + 1);
     }
 
     return 0;
@@ -64,3 +126,4 @@ int main() {
 //1
 //1
 //1
+
